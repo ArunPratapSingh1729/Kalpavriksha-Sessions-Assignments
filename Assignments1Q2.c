210a599b57b68006f5e1e588a0c2291416b78b3d
Arun Pratap Singh <arunofficial1729@gmail.com>
2025-09-28 01:26:28 +0530

    Now we can also read and can handle the write function correctly

--- Assignments1Q2.c
+++ Assignments1Q2.c
@@ -11,14 +11,60 @@ struct User
 
 void AddUser(int id, const char *name, int age)
 {
-    FILE *f = fopen("users.txt", "a");
+    FILE *f = fopen("users.txt", "r"); 
+    if (f == NULL)
+    {
+        FILE *f = fopen("users.txt", "a");
+    }
+
+    int found = 0;
+    char line[100];
+    struct User user;
+
+    if (f != NULL)
+    {
+        while (fgets(line, sizeof(line), f) != NULL)
+        {
+            sscanf(line, "%d %s %d", &user.id, user.name, &user.age);
+            if (id == user.id)
+            {
+                found = 1;
+                break;
+            }
+        }
+        fclose(f);
+    }
+
+    if (!found)
+    {
+        f = fopen("users.txt", "a"); 
+        if (f == NULL)
+        {
+            perror("Error opening file");
+            return;
+        }
+        fprintf(f, "%d %s %d\n", id, name, age);
+        fclose(f);
+    }
+}
+
+void ReadUser()
+{
+    FILE *f = fopen("users.txt", "r");
     if (f == NULL)
     {
         perror("Error ! \n");
         return;
     }
+    struct User user;
+    char line[100];
 
-    fprintf(f, "%d %s %d \n", id, name, age);
+    printf("List Of All Users \n");
+    while (fgets(line, sizeof(line), f) != NULL)
+    {
+        sscanf(line, "%d %s %d", &user.id, user.name, &user.age);
+        printf("%d , %s, %d \n", user.id, user.name, user.age);
+    }
 
     fclose(f);
 }
@@ -36,6 +82,9 @@ int main()
     AddUser(u2.id, u2.name, u2.age);
     AddUser(u3.id, u3.name, u3.age);
     // printf("Users added succesfully");
+
+    ReadUser(); // To Read The users Data
+
 
     return 0;
 }
