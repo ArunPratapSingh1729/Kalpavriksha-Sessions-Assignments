88faa6730c6a1438d1d9f47018a1cc35f1d78eaf
Arun Pratap Singh <arunofficial1729@gmail.com>
2025-10-08 02:19:01 +0530

    Updated Code More Cleaned, Better Identation, Proper Variables Names, Using Switch Case for less code handling

--- Assignments1Q1.c
+++ Assignments1Q1.c
@@ -2,118 +2,114 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(){
-
-  char entry[100];   //character array (string)  for taking input
-  char operators[50]; //operator array to store the operators
-  int numbers[50]; //numbers array to store the digits
-  int Onum = 0;    //counter for the operators
-  int Nnum = 0 ;   //counter for the numbers
-  int num = 0;     
-  printf("Enter the Input : \n");
-  fgets(entry ,100, stdin);
-
-  int len = strlen(entry);
-  for(int i =0;i<len; i++){
-
-    
-      if (isdigit(entry[i]))
-      {
-          num = num * 10 + (entry[i] - '0');
-      }
-      else if (entry[i] == '+' || entry[i] == '-' || entry[i] == '*' || entry[i] == '/' || entry[i] == '%')  
-      {
-          int j = i + 1;
-          while (j < len && (entry[j] == ' ' || entry[j] == '\n'))
-          {
-              j++; // this will skip mutliple spaces
-          }
-
-        if(j >= len || !isdigit(entry[j])){
-            printf("Invalid expression \n");
-            return 1;
-        }
-        numbers[Nnum++] = num;  //for handling the multiple digit number 
-        num = 0;
-          operators[Onum++] = entry[i];
-      }
-      else if (entry[i] == ' ' || entry[i] == '\n')
-      {
-          continue;
-      }
-      else
-      {
-          printf("Special Characters not allowed \n");
-          return 1;
-      }
-  }
-  numbers[Nnum++] = num; //for adding the last digit remaining
-
-
-for(int i=0; i<Onum; i++){
-    int nextnum = numbers[i+1];
-    if (operators[i] == '/' || operators[i] == '*' || operators[i] == '%')
-    {
-        if (operators[i] == '/' && numbers[i + 1] == 0)
-        {
-            printf("Division by zero not allowed\n");
-            return 1;
-        }
-        else if (operators[i] == '/')
-        {
-            numbers[i] = numbers[i] / numbers[i + 1];
-        }
-        else if (operators[i] == '*')
-        {
-            numbers[i] = numbers[i] * numbers[i + 1];
-        }
-        else if (operators[i] == '%')
-        {
-            numbers[i] = numbers[i] % numbers[i + 1];
-        }
-        
-        for(int j = i+1; j < Nnum-1; j++){
-            numbers[j] = numbers[j+1];
-        }
-        for(int j=i; j < Onum-1; j++){
-            operators[j] = operators[j+1];
-        }
-        Nnum --;
-        Onum --;
-        i --;
-    }
-}
-
-for (int i = 0; i < Onum; i++)
+int main()
 {
-    int nextnum = numbers[i + 1];
-    if (operators[i] == '+' || operators[i] == '-' )
-    {
-        if (operators[i] == '+')
-        {
-            numbers[i] = numbers[i] + numbers[i + 1];
-        }
-        else if (operators[i] == '-')
-        {
-            numbers[i] = numbers[i] -numbers[i + 1];
-        }
-     
 
-        for (int j = i + 1; j < Nnum - 1; j++)
+    char input[100];
+    char operator[50];
+    int number[50];
+    int numbercount = 0;
+    int operatorcount = 0;
+    int totalsum = 0;
+    int tempnum = 0;
+
+    printf("Enter the input \n");
+    fgets(input, 100, stdin);
+
+    int len = strlen(input);
+
+    for (int i = 0; i < len; i++)
+    {
+
+        char ch = input[i];
+
+        if (isdigit(ch))
         {
-            numbers[j] = numbers[j + 1];
+            tempnum = tempnum * 10 + (ch - '0');
         }
-        for (int j = i; j < Onum - 1; j++)
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%')
         {
-            operators[j] = operators[j + 1];
+            int j = i + 1;
+            while (j < len && (input[j] == ' ' || input[j] == '\n'))
+            {
+                j++; 
+            }
+
+            if (!isdigit(input[j]))
+            {
+                printf("Invalid expression \n");
+                return 1;
+            }
+            number[numbercount++] = tempnum; 
+            tempnum = 0;
+            operator[operatorcount++] = ch;
         }
-        Nnum--;
-        Onum--;
+        else if (ch == ' ' || ch == '\n')
+        {
+            continue;
+        }
+        else
+        {
+            printf("Special Characters not allowed \n");
+            return 1;
+        }
+    }
+
+    number[numbercount++] = tempnum; 
+    int nextnum = 0;
+
+    for (int i = 0; i < operatorcount; i++)
+    {
+
+        char ch = operator[i];
+        nextnum = number[i + 1];
+
+        switch (ch)
+        {
+        case '/':
+            if (nextnum == 0)
+            {
+                printf("Division by zero not allowed\n");
+                return 1;
+            }
+            number[i] = number[i] / nextnum;
+            break;
+
+        case '*':
+            number[i] = number[i] * nextnum;
+            break;
+
+        case '%':
+            number[i] = number[i] % nextnum;
+            break;
+
+        case '+':
+            number[i] = number[i] + nextnum;
+            break;
+
+        case '-':
+            number[i] = number[i] - nextnum;
+            break;
+
+        default:
+
+            break;
+        }
+
+        for (int j = i + 1; j < numbercount - 1; j++)
+        {
+            number[j] = number[j + 1];
+        }
+        for (int j = i; j < operatorcount - 1; j++)
+        {
+            operator[j] = operator[j + 1];
+        }
+        numbercount--;
+        operatorcount--;
         i--;
     }
+
+    printf("The result is %d  \n", number[0]);
+
+    return 0;
 }
-
-printf("The result is %d  \n" , numbers[0]);
-
- return 0;
-} 
